c9840eaff8e75cdd1928635938e2ed3fdad6e4c9
Luna-De-Souter <52402113+Luna-De-Souter@users.noreply.github.com>
2023-11-24 14:14:33 +0100

    Update def_contrapositives.R
    New functions, written by Mathias

--- def_contrapositives.R
+++ def_contrapositives.R
@@ -1,187 +1,209 @@
 
+#' This script defines functions for calculating contrapositive consistency 
+#' (c-consistency) and contrapositive coverage (c-coverage).
+#' 
+#' The functions are provided by Mathias Ambühl.
 
-# This script defines functions for calculating contrapositive consistency
-# and contrapositive coverage based on a cna output and the dataset from which 
-# that output was inferred.
-# The dataset should be a crisp-set dataset in the form of a dataframe.
-# Examples on how to use the functions are provided at the bottom of
-# the script.
-
-
-
-#################
-### functions ###
-#################
-
-# asf_contrapositives(ana, df) calculates contrapositive consistency and
-# contrapositive coverage for atomic solution formulas (asfs).
-# The first argument, ana, is a cna output (as returned by the function cna()).
-# The second argument, df, is the crisp-set dataset from which ana was 
-# inferred using the cna function.
-# The function returns a dataframe of asfs, as is standardly returned by the
-# asf() function in the cna package, with two additional columns, one 
-# containing the contrapositive consistency and one containing the 
-# contrapositive coverage of the asfs in the dataframe.
-
-asf_contrapositives <- function(ana, df){
-  # get the asfs of the cna output
-  asfs <- asf(ana)
-  # make a copy
-  asfs2 <- asfs
-  # get the total number of cases in the dataset
-  N <- nrow(df)
-  # get the number of cases with a positive outcome value: |Y|
-  asfs2$Y <- sapply(asfs2$outcome, function(y) sum(df[,y]))
-  # get the number of cases with a positive antecedent value and positive 
-  # outcome value: |X*Y| (coverage = |X*Y|/|Y|, so |X*Y| = coverage * |Y|)
-  asfs2$XY <- asfs2$coverage * asfs2$Y
-  # get the number of cases with a negative antecedent value and positive 
-  # outcome value: |x*Y| (|Y| = |X*Y| + |x*Y|, so |x*Y| = |Y| - |X*Y|)
-  asfs2$xY <- asfs2$Y - asfs2$XY
-  # get the number of cases with a positive antecedent value: |X|
-  # (coverage/consistency = |X|/|Y|, so coverage/consistency*|Y| = |X|)
-  asfs2$X <- asfs2$coverage/asfs2$consistency*asfs2$Y
-  # get the number of cases with a negative antecedent value: |x| (this is 
-  # equal to the total number of cases in the dataset which do not have a 
-  # positive antecedent value, so |x| = N - |X|)
-  asfs2$x_small <- N - asfs2$X
-  # get the number of cases with a negative outcome value: |y| (this is 
-  # equal to the total number of cases in the dataset which do not have a 
-  # positive outcome value, so |y| = N - |Y|)
-  asfs2$y_small <- N - asfs2$Y
-  # get the number of cases with a negative antecedent value and a negative 
-  # outcome value: |x*y| (|x| = |x*Y| + |x*y|, so |x*y| = |x| - |x*Y|)
-  asfs2$xy <- asfs2$x_small - asfs2$xY
-  # get contrapositive consistency: |x*y|/|y|
-  asfs$contrapositive_consistency <- asfs2$xy / asfs2$y_small
-  # get contrapositive coverage: |x*y|/|x|
-  asfs$contrapositive_coverage <- asfs2$xy / asfs2$x_small
-  return(asfs)
-}
-
-
-# csf_contrapositives(ana, df) calculates contrapositive consistency and
-# contrapositive coverage for complex solution formulas (csfs).
-# The first argument, ana, is a cna output (as returned by the function cna()).
-# The second argument, df, is the crisp-set dataset from which ana was 
-# inferred using the cna function.
-# The function returns a dataframe of csfs, as is standardly returned by the
-# csf() function in the cna package, with two additional columns, one 
-# containing the contrapositive consistency and one containing the 
-# contrapositive coverage of the csfs in the dataframe.
-#
-# csf_contrapositives() depends on asf_contrapositives(). So, make sure to 
-# execute the definition of both asf_contrapositives() and 
-# csf_contrapositives() before using csf_contrapositives().
-
-csf_contrapositives <- function(ana, df){
-  # get the contrapositives of the asfs of the cna output
-  asfs_contr <- asf_contrapositives(ana, df)
-  # get the csfs of the cna output
-  csfs <- csf(ana)
-  # for each csf, get its contrapositive consistency by retrieving the 
-  # contrapositive consistencies of all asfs in the csf (from asfs_contr) and 
-  # then taking the minimum of those contrapositive consistencies
-  csfs$contrapositive_consistency <- sapply(csfs$condition, function(x) {
-    if (grepl("\\*", x)){
-      parts <- strsplit(x, "\\)\\*\\(")[[1]]
-      parts <- trimws(parts)
-      parts <- gsub("\\(", "", parts)
-      parts <- gsub("\\)", "", parts)
-      parts <- sapply(parts, function(y) asfs_contr$contrapositive_consistency[asfs_contr$condition == y])
-      return(min(parts))
-    } else {
-      return(asfs_contr$contrapositive_consistency[asfs_contr$condition == x])
-      }})
-  # for each csf, get its contrapositive coverage by retrieving the 
-  # contrapositive coverages of all asfs in the csf and then taking the 
-  # minimum of those contrapositive coverages
-  csfs$contrapositive_coverage <- sapply(csfs$condition, function(x) {
-    if (grepl("\\*", x)){
-      parts <- strsplit(x, "\\)\\*\\(")[[1]]
-      parts <- trimws(parts)
-      parts <- gsub("\\(", "", parts)
-      parts <- gsub("\\)", "", parts)
-      parts <- sapply(parts, function(y) asfs_contr$contrapositive_coverage[asfs_contr$condition == y])
-      return(min(parts))
-      } else {
-      return(asfs_contr$contrapositive_coverage[asfs_contr$condition == x])
-        }
-    })
-  return(csfs)
-}
-
-
-# msc_contrapositives(ana, df) calculates contrapositive consistency and
-# contrapositive coverage for minimally sufficient conditions (mscs).
-# The first argument, ana, is a cna output (as returned by the function cna()).
-# The second argument, df, is the crisp-set dataset from which ana was 
-# inferred using the cna function.
-# The function returns a dataframe of mscs, as is standardly returned by the
-# msc() function in the cna package, with two additional columns, one 
-# containing the contrapositive consistency and one containing the 
-# contrapositive coverage of the mscs in the dataframe.
-
-msc_contrapositives <- function(ana, df){
-  # get the mscs of the cna output
-  mscs <- msc(ana)
-  # make a copy
-  mscs2 <- mscs
-  # get the total number of cases in the dataset
-  N <- nrow(df)
-  # get the number of cases with a positive outcome value: |Y|
-  mscs2$Y <- sapply(mscs2$outcome, function(y) sum(df[,y]))
-  # get the number of cases with a positive antecedent value and positive 
-  # outcome value: |X*Y| (coverage = |X*Y|/|Y|, so |X*Y| = coverage * |Y|)
-  mscs2$XY <- mscs2$coverage * mscs2$Y
-  # get the number of cases with a negative antecedent value and positive 
-  # outcome value: |x*Y| (|Y| = |X*Y| + |x*Y|, so |x*Y| = |Y| - |X*Y|)
-  mscs2$xY <- mscs2$Y - mscs2$XY
-  # get the number of cases with a positive antecedent value: |X|
-  # (coverage/consistency = |X|/|Y|, so coverage/consistency*|Y| = |X|)
-  mscs2$X <- mscs2$coverage/mscs2$consistency*mscs2$Y
-  # get the number of cases with a negative antecedent value: |x| (this is 
-  # equal to the total number of cases in the dataset which do not have a 
-  # positive antecedent value, so |x| = N - |X|)
-  mscs2$x_small <- N - mscs2$X
-  # get the number of cases with a negative outcome value: |y| (this is 
-  # equal to the total number of cases in the dataset which do not have a 
-  # positive outcome value, so |y| = N - |Y|)
-  mscs2$y_small <- N - mscs2$Y
-  # get the number of cases with a negative antecedent value and a negative 
-  # outcome value: |x*y| (|x| = |x*Y| + |x*y|, so |x*y| = |x| - |x*Y|)
-  mscs2$xy <- mscs2$x_small - mscs2$xY
-  # get contrapositive consistency: |x*y|/|y|
-  mscs$contrapositive_consistency <- mscs2$xy / mscs2$y_small
-  # get contrapositive coverage: |x*y|/|x|
-  mscs$contrapositive_coverage <- mscs2$xy / mscs2$x_small
-  return(mscs)
-}
-
-
-
-################
-### examples ###
-################
-
+# load cna library
 library(cna)
 
-# Run a cna analysis.
-ana <- cna(d.irrigate, con = 0.8, cov = 0.8)
 
-# Calculate contrapositive consistency and contrapositive coverage of asfs,
-# using the asf_contrapositives function.
-asfs_contr <- asf_contrapositives(ana, d.irrigate)
-asfs_contr
+# ===== Description of the functions =====
 
-# Calculate contrapositive consistency and contrapositive coverage of csfs,
-# using the csf_contrapositives function.
-csfs_contr <- csf_contrapositives(ana, d.irrigate)
-csfs_contr
-
-# Calculate contrapositive consistency and contrapositive coverage of mscs,
-# using the msc_contrapositives function.
-mscs_contr <- msc_contrapositives(ana, d.irrigate)
-mscs_contr
+#' The functions condTbl(), asf() and csf() will be most useful for cna 
+#' users.
+#' These functions take the same main arguments as the functions condTbl(), 
+#' asf() and csf() from the regular cna library on CRAN.
+#' They return the same results as their counterparts from the regular cna 
+#' library, but with two additional columns containing contrapositive 
+#' consistency (c-consistency) and contrapositive coverage (c-coverage).
+#' 
+#' Examples on how to use condTbl(), asf() and csf() as defined in this script 
+#' are provided at the bottom of this script.
 
 
+#' The functions contrapositives() and add_contrapositives() are helper 
+#' functions: they will usually not be used directly by cna users but need to 
+#' be defined because the functions condTbl(), asf() and csf() as defined in 
+#' this script rely on them.
+#' 
+#' contrapositives() takes two arguments:
+#' * cond : a character vector containing conditions. Only asfs or csfs in 
+#' disjunctive normal form can be processed by contrapositives()
+#' * d : a data.frame or configTable
+#' The return value is a data.frame with two columns named "c_consistency" and 
+#' "c_coverage".
+#' 
+#' add_contrapositives() takes two arguments, too:
+#' * tbl : a condTbl, the output of functions contTbl(), asf(), csf()
+#' * d : the data that was used to create tbl
+#' It adds the columns c_consistency and c_coverage to the table and
+#' rearranges the columns such that c_consistency and c_coverage are displayed 
+#' before consistency and coverage.
+
+#' The function fourfold will usually not be used directly by cna users, but 
+#' may be helpful to some.
+#' 
+#' fourfold() takes a condition or asf and data (crisp-set or multi-value) 
+#' arguments and prints a fourfold table (confusion matrix) for each condition.
+#' 
+
+
+# ===== Defining of the functions =====
+
+# Contrapositives()
+contrapositives <- function(cond, d){
+  if (!inherits(d, "configTable")) d <- configTable(d)
+  cti <- ctInfo(d)
+  condTypes <- as.vector(cna:::.qcondType(cond, values = colnames(cti$scores), cti$type))
+  is_atomic <- !is.na(condTypes) & condTypes == "stdAtomic"
+  is_complex <- !is.na(condTypes) & condTypes == "stdComplex"
+  ncond <- length(cond)
+  out <- data.frame(c_consistency = rep(NA_real_, ncond), 
+                    c_coverage = rep(NA_real_, ncond))
+  # apply negation&invertion for asf components of complex conditions
+  if (any(is_complex)){
+    asfs <- extract_asf(cond[is_complex])
+    # complex with only one asf are treated as asf:
+    len2plus <- lengths(asfs) > 1
+    unlistedAsfs <- unlist(asfs)
+    negUnlisted <- paste0("!(", rhs(unlistedAsfs), ")->!(", lhs(unlistedAsfs), ")")
+    negComplex <- split.default(negUnlisted, rep(seq_along(asfs), lengths(asfs)))
+    negComplex <- paste0(ifelse(len2plus, "(", ""),
+                         cna:::C_mconcat(negComplex, sep = ")*("), 
+                         ifelse(len2plus, ")", ""))
+    out[is_complex, ] <- cna::condTbl(negComplex, d)[c("consistency", "coverage")]
+  }
+  # negate both sides of atomic conditions and invert them
+  if (any(is_atomic)){
+    negAtomic <- paste0("!(", rhs(cond[is_atomic]), ")->!(", lhs(cond[is_atomic]), ")")
+    out[is_atomic, ] <- cna::condTbl(negAtomic, d)[c("consistency", "coverage")]
+  }
+  out  
+}
+
+# add_contrapositives()
+add_contrapositives <- function(tbl, d){
+  if (!inherits(d, "configTable")) d <- configTable(d)
+  cond <- tbl$condition
+  tbl[c("c_consistency", "c_coverage")] <- contrapositives(cond, d)
+  tbl[union(intersect(c("outcome", "condition", "c_consistency", "c_coverage"), 
+                      names(tbl)), 
+            names(tbl))]
+}
+# condTbl()
+condTbl <- function(cond, d){
+  co <- condition(cond, d)  
+  std <- attr(co, "info")$condType %in% c("stdAtomic", "stdComplex")
+  out <- as.condTbl(co)
+  out$consistency[!std] <- out$coverage[!std] <- NA_real_
+  add_contrapositives(out, d)
+}
+# asf()
+asf <- function(x, ...){
+  stopifnot(inherits(x, "cna"))
+  add_contrapositives(cna::asf(x, ...), x$configTable)
+}
+# csf()
+csf <- function(x, ...){
+  stopifnot(inherits(x, "cna"))
+  add_contrapositives(cna::csf(x, ...), x$configTable)
+}
+
+# fourfold()
+fourfold <- function(cond, d){
+  if (!inherits(d, "configTable")) d <- configTable(d)
+  condTypes <- as.vector(getCondType(cond, ctInfo(d)))
+  stopifnot(attr(d, "type") %in% c("cs", "mv"))
+  if (!all(grepl("atomic", condTypes, ignore.case = TRUE))){
+    stop("fourfold() accepts atomic conditions only.")
+  }
+  out <- lapply(condition(cond, d), .fourfold1)
+  out <- array(unlist(out), dim = c(2, 2, length(out)), 
+               dimnames = list(c("TRUE", "FALSE"), c("TRUE", "FALSE"), names(out)))
+  out[is.na(out)] <- 0L
+  out
+}
+# .fourfold1() is an auxiliary function used in fourfold
+.fourfold1 <- function(cond1){
+  nn <- attr(cond1, "n")
+  cond1 <- lapply(cond1, as.logical)
+  out <- tapply(nn, cond1, sum)[2:1, 2:1]
+  out  
+}
+
+
+# ===== examples =====
+
+#' Before running the examples below, make sure to execute all previous 
+#' lines in this script to correctly define the functions
+#' condTbl(), asf() and csf().
+
+
+### crisp-set ###
+
+#' Run a crisp-set CNA analysis.
+ana <- cna(d.women, con = 0.7, cov = 0.7)
+
+#' Apply the new asf() function to display the returned atomic solution 
+#' formulas (i.e. single-outcome models) with additional columns for 
+#' c-consistency and c-coverage.
+asfs <- asf(ana)
+asfs
+
+#' Apply the new csf() function to display the returned complex solution 
+#' formulas (i.e. multiple-outcome models) with additional columns for 
+#' c-consistency and c-coverage.
+csfs <- csf(ana)
+csfs
+
+#' Apply the new condTbl() function to display the c-consistency and 
+#' c-coverage of the minimally sufficient conditions.
+mscs <- msc(ana)
+condTbl(mscs, d.women)
+
+
+### multi-value ###
+
+#' Run a multi-value CNA analysis.
+ana <- cna(d.pban, con = 0.85, cov = 0.85)
+
+#' Apply the new asf() function to display the returned atomic solution 
+#' formulas (i.e. single-outcome models) with additional columns for 
+#' c-consistency and c-coverage.
+asfs <- asf(ana)
+asfs
+
+#' Apply the new csf() function to display the returned complex solution 
+#' formulas (i.e. multiple-outcome models) with additional columns for 
+#' c-consistency and c-coverage.
+csfs <- csf(ana)
+csfs
+
+#' Apply the new condTbl() function to display the c-consistency and 
+#' c-coverage of the minimally sufficient conditions.
+mscs <- msc(ana)
+print(condTbl(mscs, d.pban), 100)
+
+
+### fuzzy-set ###
+
+#' Run a fuzzy-set CNA analysis.
+ana <- cna(d.pacts, con = 0.75, cov = 0.75)
+
+#' Apply the new asf() function to display the returned atomic solution 
+#' formulas (i.e. single-outcome models) with additional columns for 
+#' c-consistency and c-coverage.
+asfs <- asf(ana)
+asfs
+
+#' Apply the new csf() function to display the returned complex solution 
+#' formulas (i.e. multiple-outcome models) with additional columns for 
+#' c-consistency and c-coverage.
+csfs <- csf(ana)
+csfs
+
+#' Apply the new condTbl() function to display the c-consistency and 
+#' c-coverage of the minimally sufficient conditions.
+mscs <- msc(ana)
+condTbl(mscs, d.pacts)
